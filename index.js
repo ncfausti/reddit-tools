0661e0d89820011f512b1e4afacc7a3b43a94103
Nick Fausti <ncfausti@gmail.com>
2021-10-25 18:42:19 -0400

    Update recurseComments to return list of auther:comment objects.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function recurseComments(comments) {
-  comments.forEach(comment => { 
-      console.log(comment.data.author)
-      if (comment.data.replies) {
-        recurseComments(comment.data.replies.data.children)
-      }
-   })
+function recurseComments(comments, accumulator) {
+  comments.forEach(comment => {
+    if (comment.data.author in accumulator) {
+      accumulator.push({ author: comment.data.author, comment });
+    }
+    else {
+      accumulator.push({ author: comment.data.author, comment });
+    }
+
+    if (comment.data.replies) {
+      recurseComments(comment.data.replies.data.children, accumulator)
+    }
+  })
+  return accumulator;
 }
